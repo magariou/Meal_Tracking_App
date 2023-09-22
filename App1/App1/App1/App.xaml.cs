05f69064bc8c0e610d49dfb1996607808b0ca464
MagariouDiallo <129006223+MagariouDiallo@users.noreply.github.com>
2023-09-22 10:26:56 -0400

    First App Commit

---
+++ App1/App1/App1/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using App1.ViewsModels;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new HomePage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+
+        }
+    }
+}
---
+++ App1/App1/App1/Views/HomePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using App1.ViewsModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HomePage : ContentPage
+	{
+		public HomePage ()
+		{
+			InitializeComponent ();
+
+			BindingContext = new HomePageViewModel();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
